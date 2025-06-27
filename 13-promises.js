dd11e0234154a34dd64fe0739a988bd45a6b6a71
vasusharrma <sharmavashu099@gmail.com>
2025-06-27 22:08:53 +0530

    promises and callbacks

---
+++ 13-promises.js
@@ -0,0 +1,25 @@
+function uploadSongs() {
+  return new Promise((resolve, reject) => {
+    const depender = Math.floor(Math.random() * 10);
+    if (depender > 4) {
+      setTimeout(() => {
+        resolve("hello folks");
+      }, 0);
+    } else {
+      setTimeout(() => {
+        reject(new Error("we got error folks"));
+      }, 0);
+    }
+  });
+}
+
+const songs = uploadSongs();
+
+console.log(songs);
+setTimeout(() => {
+  console.log("status : ", songs);
+}, 0);
+
+songs.then((data) => {
+  console.log(data);
+});
